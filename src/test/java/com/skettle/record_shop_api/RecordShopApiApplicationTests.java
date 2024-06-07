e3f779c054cf8e7642cf895414ea7bb8ec6a74cb
Sam Kettle <sam.kettle3@icloud.com>
2024-06-07 10:22:26 +0100

    Project initialised

---
+++ src/test/java/com/skettle/record_shop_api/RecordShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skettle.record_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
