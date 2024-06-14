396bf2b478ad8ee40256906b8438f11d7bd0cf6e
Sam Kettle <sam.kettle3@icloud.com>
2024-06-14 16:19:11 +0100

    feat: only DELETE and PUT methods require authentication

--- src/main/java/com/skettle/record_shop_api/security/SecurityConfig.java
+++ src/main/java/com/skettle/record_shop_api/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.skettle.record_shop_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,9 +10,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
+                .requestMatchers(HttpMethod.DELETE, "/albums/**").authenticated()
+                .requestMatchers(HttpMethod.PUT, "/albums/**").authenticated()
                 .and()
                 .oauth2Login();
         return http.build();
